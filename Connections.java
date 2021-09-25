73a96d6687c16de76998b0a3e519cf607018f8ce
RahafShami2 <75248980+RahafShami2@users.noreply.github.com>
2021-09-25 23:05:03 +0300

    Update Connections.java

--- Connections.java
+++ Connections.java
@@ -2,17 +2,72 @@ package edu.najah.cap.ass2;
 import java.util.ArrayList;
 
 public class Connections{
+    
+    private static Connections HTTP;
+    private static Connections SSH;
+    private static Connections TELNET;
+    private static Connections SCP;
+    private static Connections FTP;
+
     public static final String http=null;
      public static final String ssh=null;
      public static final String telten=null;
      public static final String scp=null;
      public static final String ftp=null;
-
-     public static Connections getInstance(String HTTP , String SSH,String TELNET , String CSP , String FTP){
+    
+    public static int count=0;
+    public static ArrayList<String>List= new ArrayList<String>();
+    
+     public static Connections getInstance(String ConnPro){
+         if (count==3){
+             system.out.println("you have 3 connections");
      return null;
      }
-     public static boolean release(String HTTP , String SSH,String TELNET , String CSP , String FTP ){
-         
+         if (ConnPro =="HTTP"){
+             if (HTTP ==null){
+                 HTTP= new Connections();
+                 List.add("HTTP");
+                 count++;
+             }
+             return HTTP;
+         }
+             else if (ConnPro =="SSH"){
+             if (SSH ==null){
+                 SSH= new Connections();
+                 List.add("SSH");
+                 count++;
+             }
+             return SSH;
+             }
+           else if (ConnPro =="TELNET"){
+             if (TELNET ==null){
+                 TELNET= new Connections();
+                 List.add("TELNET");
+                 count++;
+             }
+             return TELNET;
+             }
+         else if (ConnPro =="CSP"){
+             if (CSP ==null){
+                 CSP= new Connections();
+                 List.add("CSP");
+                 count++;
+             }
+             return CSP;
+             }
+          else if (ConnPro =="FTP"){
+             if (FTP ==null){
+                 FTP= new Connections();
+                 List.add("FTP");
+                 count++;
+             }
+             return FTP;
+             }     
+         }
+    
+    
+     public static boolean release(String ConnPro){
+         if (count>3)
          return false ;
      }
      public static ArrayList<String> getCurrentConnection(){
