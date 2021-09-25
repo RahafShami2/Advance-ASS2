266dda6f6663a7504355352e5180ab5969aecaed
RahafShami2 <75248980+RahafShami2@users.noreply.github.com>
2021-09-25 22:17:40 +0300

    Update Single.java

--- Single.java
+++ Single.java
@@ -3,15 +3,35 @@ import java.util.ArrayList;
 
 public class Single{
     	public static void main(String[] args) {
-        boolean isReleased = Connections.release(Connections.http || Connections.scp || connections.telnet || connections.ssh||connections.ftp); 
+		
+		Connection httpConnection1 = Connections.getInstance(Connections.http); 
+		Connection httpConnection2 = Connections.getInstance(Connections.http); 
+		Connection scpConnection1 = Connections.getInstance("scp"); 
+		Connection telnetConnection1 = Connections.getInstance("telnet"); 
+		Connection sshConnection1 = Connections.getInstance("ssh"); 
+		Connection scpConnection1 = Connections.getInstance("scp"); 
+
+        boolean isReleased = Connections.release(Connections.http); 
 		if(Connection == null) {
 			return true;
 		}
-        Connection httpConnection1 = Connections.getInstance(Connections.http); 
-		Connection httpConnection2 = Connections.getInstance(Connections.http); 
+		
+		Connection ftpConnection1 = Connections.getInstance("ftp"); 
+
 		if(httpConnection1 == httpConnection2) { 
             return true;
 		}; 
+		
 		ArrayList<String> current= Connections.getCurrentConnection(); 
-        httpConnection1.send("My message");
+		system.out.println(current);
+		
+             scpConnection1.send("hi");
+            ftpConnection1.send("hi");	
         }
+
+
+		
+
+	}
+
+}
